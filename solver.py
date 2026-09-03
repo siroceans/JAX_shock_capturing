3ceb9d58396c313c8157c88e79c35aa9ccf2ae30
Jorge Mares <kokomares@gmail.com>
2026-09-03 13:35:18 -0500

    grid initialization done

--- solver.py
+++ solver.py
@@ -1,5 +1,31 @@
 import jax
 import jax.numpy as jnp
-import numpy as np
+
+# setting default to double fpn
+jax.config.update("jax_enable_x64", True)
+
+## Solving Sod's shock tube problem using different flux approaches in FVM
+# Jorge Luis Mares Zamora
+# Last worked on:
+
+def initializeField(rho_right, rho_left, P_right, P_left, x_discontinuity, n_x, gamma, L, u_0):
+    dx = L / n_x # remember that nx is the no. of cells, not the no. of edges!!
+    U = jnp.zeros((n_x, 3)) # leading axis is the no. of cells that we have!
+    centroid_x = jnp.arange(0 + dx/2, L, dx) # array of centroid locations
+
+    # creating rho, P, and a arrays
+    rho = jnp.where(centroid_x < x_discontinuity, rho_left, rho_right)
+    P = jnp.where(centroid_x < x_discontinuity, P_left, P_right)
+    a = jnp.sqrt(P * gamma / rho)
+
+    # filling in the U matrix
+    U = U.at[:, 0].set(rho)
+    U = U.at[:, 1].set(rho * u_0)
+    U = U.at[:, 2].set(rho * (a**2/(gamma * (gamma-1)) + 1/2 * u_0**2))
+
+    # creating maximum wave speed array! (for cfl condition)
+    c = jnp.abs(u_0) + a
+    return U, c
 
 
+def shockTubeSolver():
